a22eacfe2e48921998f1096c88ee8ff970a7201b
余腾靖 <ytj2713151713@gmail.com>
2024-10-01 17:59:32 +0800

    reproduce issue

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+
+import { codeInspectorPlugin } from 'code-inspector-plugin';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '*',
+            },
+        ],
+    },
+    webpack: (config) => {
+        config.plugins.push(
+            codeInspectorPlugin({ bundler: 'webpack', hideConsole: true, hideDomPathAttr: true }),
+        );
+        return config;
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
 };
 
 export default nextConfig;
